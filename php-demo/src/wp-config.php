bafa98661a6ffdfce6e1fb309ab85c48d08283fd
colin <colin@123.com>
2019-11-30 00:06:26 +0800

    add php

--- php-demo/src/wp-config.php
+++ php-demo/src/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'test');
+define('DB_NAME', 'wordpress');
 
 /** MySQL数据库用户名 */
 define('DB_USER', 'root');
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '123456');
 
 /** MySQL主机 */
-define('DB_HOST', '192.168.0.34');
+define('DB_HOST', '192.168.149.132');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
